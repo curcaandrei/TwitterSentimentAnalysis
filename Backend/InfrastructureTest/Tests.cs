e16896899167e4ab18720101bfcf8db99dbaef4e
Andrei Curca <Andrei.Curca@endava.com>
2021-11-30 01:28:37 +0200

    Test Project

---
+++ Backend/InfrastructureTest/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace InfrastructureTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
